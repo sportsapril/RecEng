2e67cf9946c820ac38f68acff85418317986b3ca
206612586 <april.xu@nbcuni.com>
2019-10-22 13:01:05 -0700

    wire up the getRecommendationAll piece

--- getRecommendAll.py
+++ getRecommendAll.py
@@ -2,26 +2,34 @@ import numpy as np
 import pandas as pd
 from scipy.sparse import coo_matrix
 import sys
-import urllib.request
+# import urllib.request
 
 #specify base path and job_ID to retrieve model output
-base_path = '/Users/april.xu/recommendationEngine/wals_ml_engine/'
-job_ID = 'wals_ml_local_20190428_172743'
+pwd1 = '/Users/aprilxu/Documents/GitHub/RecEng'
+
+print('-------------------------')
+print("Path: " + pwd1)
+print('-------------------------')
+
+file_path = pwd1 + '/'
+base_path = pwd1 + '/'
+sys.path.append(base_path +'util/')
+sys.path.append(base_path +'model/')
 
 #add to path so that you can load defined packages
-sys.path.append(base_path+ '/trainer/')
-import model as md
+import algo as model
+import utility as utl
 
-col_factor = np.load(base_path + 'jobs/' + job_ID + '/model/col.npy') # 9724 x 5
-row_factor = np.load(base_path + 'jobs/' + job_ID + '/model/row.npy') # 610 x 5
-item_ID_mapping_dd_raw = pd.read_csv(base_path + 'jobs/' + job_ID + '/model/item_ID_mapping_dd.csv')
-movie_spec = pd.read_csv('/Users/april.xu/recommendationEngine/data/links.csv')
+col_factor = np.load(base_path + 'job/' + 'model/col.npy') # 9724 x 5
+row_factor = np.load(base_path + 'job/' + 'model/row.npy') # 610 x 5
+item_ID_mapping_dd_raw = pd.read_csv(base_path + 'job/' + 'model/item_ID_mapping_dd.csv')
+movie_spec = pd.read_csv(base_path + 'data/Dataset_LatestSmall/links.csv')
 
 user_idx_array = range(row_factor.shape[0])
 
 headers = ['user_id', 'item_id', 'rating', 'timestamp']
 header_row = 0
-input_file = '/Users/april.xu/recommendationEngine/data/ratings.csv'
+input_file = base_path + 'data/Dataset_LatestSmall/ratings.csv'
 delimiter = ','
 ratings_df = pd.read_csv(input_file,
                        sep=delimiter,
@@ -53,7 +61,7 @@ for i in user_idx_array:
     # get recommendations for this user
     user_rated = list(item_ID_mapping_dd_orig.loc[list(topN['item_id'])]['item_rebased_ID'])
     
-    recommendations_rebased, recommendattions_prob = md.generate_recommendations(i, 
+    recommendations_rebased, recommendattions_prob = utl.generate_recommendations(i, 
                                                                                  user_rated, 
                                                                                  row_factor, 
                                                                                  col_factor, 
@@ -69,3 +77,5 @@ for i in user_idx_array:
     rec['user_id'] = client_id
     
     recommended_topN = pd.concat([recommended_topN, rec])
+
+recommended_topN.to_csv(base_path + 'output/recommended_topN.csv')
