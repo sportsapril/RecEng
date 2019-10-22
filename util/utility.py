25a10d7c9c7c874993d1a8e69e1900d33a5de49b
206612586 <april.xu@nbcuni.com>
2019-10-22 08:05:59 -0700

    intend

--- util/utility.py
+++ util/utility.py
@@ -38,69 +38,69 @@ import tensorflow as tf
 #def _ratings_train_and_test(hasHeader, delimiter, input_file):
 
 def split_train_and_test(args, input_file):
-headers = ['user_id', 'item_id', 'rating', 'timestamp']
-header_row = 0 if args['headers'] else None
-ratings_df = pd.read_csv(input_file,
-                       sep=args['delimiter'],
-                       names=headers,
-                       header=header_row,
-                       dtype={
-                           'user_id': np.int32,
-                           'item_id': np.int32,
-                           'rating': np.float32,
-                           'timestamp': np.float32,
-                       })
+  headers = ['user_id', 'item_id', 'rating', 'timestamp']
+  header_row = 0 if args['headers'] else None
+  ratings_df = pd.read_csv(input_file,
+                         sep=args['delimiter'],
+                         names=headers,
+                         header=header_row,
+                         dtype={
+                             'user_id': np.int32,
+                             'item_id': np.int32,
+                             'rating': np.float32,
+                             'timestamp': np.float32,
+                         })
 
-np_users = ratings_df.user_id.as_matrix()
-np_items = ratings_df.item_id.as_matrix()
-unique_users = np.unique(np_users)
-unique_items = np.unique(np_items)
+  np_users = ratings_df.user_id.as_matrix()
+  np_items = ratings_df.item_id.as_matrix()
+  unique_users = np.unique(np_users)
+  unique_items = np.unique(np_items)
 
-n_users = unique_users.shape[0]
-n_items = unique_items.shape[0]
+  n_users = unique_users.shape[0]
+  n_items = unique_items.shape[0]
 
-# make indexes for users and items if necessary
-max_user = unique_users[-1]
-max_item = unique_items[-1]
-if n_users != max_user or n_items != max_item:
-# make an array of 0-indexed unique user ids corresponding to the dataset
-# stack of user ids
-z = np.zeros(max_user+1, dtype=int)
-z[unique_users] = np.arange(n_users)
-u_r = z[np_users]
+  # make indexes for users and items if necessary
+  max_user = unique_users[-1]
+  max_item = unique_items[-1]
+  if n_users != max_user or n_items != max_item:
+    # make an array of 0-indexed unique user ids corresponding to the dataset
+    # stack of user ids
+    z = np.zeros(max_user+1, dtype=int)
+    z[unique_users] = np.arange(n_users)
+    u_r = z[np_users]
 
-# make an array of 0-indexed unique item ids corresponding to the dataset
-# stack of item ids
-z = np.zeros(max_item+1, dtype=int)
-z[unique_items] = np.arange(n_items)
-i_r = z[np_items]
+    # make an array of 0-indexed unique item ids corresponding to the dataset
+    # stack of item ids
+    z = np.zeros(max_item+1, dtype=int)
+    z[unique_items] = np.arange(n_items)
+    i_r = z[np_items]
 
-# construct the ratings set from the three stacks
-np_ratings = ratings_df.rating.as_matrix()
-ratings = np.zeros((np_ratings.shape[0], 3), dtype=object)
-ratings[:, 0] = u_r
-ratings[:, 1] = i_r
-ratings[:, 2] = np_ratings
-else:
-ratings = ratings_df.as_matrix(['user_id', 'item_id', 'rating'])
-# deal with 1-based user indices
-ratings[:, 0] -= 1
-ratings[:, 1] -= 1
+    # construct the ratings set from the three stacks
+    np_ratings = ratings_df.rating.as_matrix()
+    ratings = np.zeros((np_ratings.shape[0], 3), dtype=object)
+    ratings[:, 0] = u_r
+    ratings[:, 1] = i_r
+    ratings[:, 2] = np_ratings
+  else:
+    ratings = ratings_df.as_matrix(['user_id', 'item_id', 'rating'])
+    # deal with 1-based user indices
+    ratings[:, 0] -= 1
+    ratings[:, 1] -= 1
 
-item_ID_mapping = pd.concat([pd.DataFrame(np_items),pd.DataFrame(ratings[:,1])],axis=1)
-item_ID_mapping.columns = ['item_original_ID','item_rebased_ID']
+    item_ID_mapping = pd.concat([pd.DataFrame(np_items),pd.DataFrame(ratings[:,1])],axis=1)
+    item_ID_mapping.columns = ['item_original_ID','item_rebased_ID']
 
-item_ID_mapping_dd = item_ID_mapping.drop_duplicates()
-item_ID_mapping_dd = item_ID_mapping_dd.set_index('item_original_ID')
+    item_ID_mapping_dd = item_ID_mapping.drop_duplicates()
+    item_ID_mapping_dd = item_ID_mapping_dd.set_index('item_original_ID')
 
-#item_ID_mapping_dd.to_csv(+'item_ID_mapping_dd.csv')
+  #item_ID_mapping_dd.to_csv(+'item_ID_mapping_dd.csv')
 
 
 
-tr_sparse, test_sparse = _create_sparse_train_and_test(ratings,
-                                                     n_users, n_items)
+  tr_sparse, test_sparse = _create_sparse_train_and_test(ratings,
+                                                       n_users, n_items)
 
-return ratings[:, 0], ratings[:, 1], tr_sparse, test_sparse, item_ID_mapping_dd
+  return ratings[:, 0], ratings[:, 1], tr_sparse, test_sparse, item_ID_mapping_dd
 
 def train_model(args, tr_sparse):
   """Instantiate WALS model and use "simple_train" to factorize the matrix.
@@ -223,14 +223,14 @@ def generate_recommendations(user_idx, user_rated, row_factor, col_factor, k):
   return recommended_items, pred_ratings[recommended_items]
 
   def validate_file(input_file):
-  """
-  Ensure the training ratings file is stored locally.
-  """
-  if input_file.startswith('gs:/'):
-    input_path = os.path.join('/tmp/', str(uuid.uuid4()))
-    os.makedirs(input_path)
-    tmp_input_file = os.path.join(input_path, os.path.basename(input_file))
-    sh.gsutil("cp", "-r", input_file, tmp_input_file)
-    return tmp_input_file
-  else:
-    return input_file
+    """
+    Ensure the training ratings file is stored locally.
+    """
+    if input_file.startswith('gs:/'):
+      input_path = os.path.join('/tmp/', str(uuid.uuid4()))
+      os.makedirs(input_path)
+      tmp_input_file = os.path.join(input_path, os.path.basename(input_file))
+      sh.gsutil("cp", "-r", input_file, tmp_input_file)
+      return tmp_input_file
+    else:
+      return input_file
